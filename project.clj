1d076d9f9af6a0316bdf67d7221f2189c16949e2
paomian <xpaomian@gmail.com>
2015-01-21 18:40:19 +0800

    Update description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-diamond "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.paomian/clj-diamond "0.1.0"
+  :description "diamond client clojure wrap"
+  :url "https://github.com/leancloud/clj-diamond"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
