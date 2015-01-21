53c047d3fa9d060c18905e98c67c91e68243fbc3
paomian <xpaomian@gmail.com>
2015-01-21 16:00:42 +0800

    Update config

--- project.clj
+++ project.clj
@@ -5,5 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.taobao.diamond/diamond-client "2.0.5.4.taocode-SNAPSHOT"]
-                 [org.clojure/tools.logging "0.3.1"]]
+                 [org.clojure/tools.logging "0.3.1"]
+                 ;;[commons-httpclient/commons-httpclient "3.1"]
+                 ;;[commons-logging/commons-logging "1.2"]
+                 ;;[commons-io/commons-io "2.4"]
+                 ;;[commons-lang/commons-lang "2.6"]
+                 ;;[com.fasterxml.jackson.core/jackson-core "2.5.0"]
+                 ;;[org.codehaus.jackson/jackson-mapper-asl "1.9.13"]
+                 ;;[log4j/log4j "1.2.17"]
+                 #_[junit/junit "4.12"]]
+  ;:java-source-paths ["src/java"]
   :main clj-diamond.core)
--- src/clj_diamond/core.clj
+++ src/clj_diamond/core.clj
@@ -1,18 +1,87 @@
 (ns clj-diamond.core
   (:require [clojure.tools.logging :as log]
             [clojure.pprint :as pp])
-  (:import (com.taobao.diamond.manager.impl DefaultDiamondManager)
+  (:import (java.util Properties)
+           (java.io FileInputStream File)
+           (com.taobao.diamond.manager.impl DefaultDiamondManager)
            (com.taobao.diamond.manager ManagerListener)
            (com.taobao.diamond.client DiamondConfigure)))
 
 (def status (atom {}))
 
 (def gconf :conf)
+
 (def gmger :manager)
+
+(defn get-property
+  [^Properties prop ^String key]
+  (.getProperty prop key))
+(defn get-property-bool
+  [^Properties prop ^String key]
+  (when-let [p (get-property prop key)]
+    (Boolean/valueOf p)))
+
+(defn get-property-num
+  [^Properties prop ^String key]
+  (when-let [p (get-property prop key)]
+    (Integer/valueOf p)))
+
+(defn update-conf*
+  []
+  (let [prop (Properties.)
+        fis (try (FileInputStream. "./conf.properties")
+                 (catch Exception e
+                   (log/info "conf not exists")))
+        _ (.load prop fis)
+        default-conf {:polling-interval-time 15
+                      #_:domain-name-list
+                      :user-flow-control true
+                      :once-timeout 2000
+                      :recv-wait-time 10000
+                      :scheduled-threadpool 1
+                      :config-server-address "127.0.0.1"
+                      :config-server-port 8080
+                      :retrieve-data-retry-times 2}
+        pconf (fn [map key fun strkey]
+                (if-let [c (fun prop strkey)]
+                  (assoc map key c)
+                  map))]
+    (if-not fis
+      default-conf
+      (let [updated-conf
+            (-> default-conf
+                (pconf :polling-interval-time get-property-num "pollingintervaltime")
+                (pconf :user-flow-control get-property-bool "userflowcontrol")
+                (pconf :once-timeout get-property-num "oncetimeout")
+                (pconf :recv-wait-time get-property-num "recvwaittime")
+                (pconf :scheduled-threadpool get-property-num "scheduledthreadpool")
+                (pconf :config-server-address get-property "configserveraddress")
+                (pconf :config-server-port get-property-num "configserverport")
+                (pconf :retrieve-data-retry-times get-property-num "retrievedataretrytimes"))]
+        (.close fis)
+        updated-conf))))
+
+(defn update-conf!
+  [^DiamondConfigure manager]
+  (let [conf (.getDiamondConfigure manager)
+        _ (println conf)
+        mapconf (update-conf*)]
+    (pp/pprint mapconf)
+    (doto conf
+      (.setPollingIntervalTime (:polling-interval-time mapconf))
+      (.setUseFlowControl (:user-flow-control mapconf))
+      (.setOnceTimeout (:once-timeout mapconf))
+      (.setReceiveWaitTime (:recv-wait-time mapconf))
+      (.setScheduledThreadPoolSize (:scheduled-threadpool mapconf))
+      (.setConfigServerAddress (:config-server-address mapconf))
+      (.setConfigServerPort (:config-server-port mapconf))
+      (.setRetrieveDataRetryTimes (:retrieve-data-retry-times mapconf)))))
 
 (defn update-status [group dataid conf]
   (swap! status
          assoc-in (map keyword [group dataid]) conf))
+
+(defn config [^DiamondConfigure conf])
 
 (defn register-manager
   "register manager"
@@ -23,6 +92,7 @@
                     (getExecutor [this] nil)
                     (receiveConfigInfo [this configinfo]
                       (update-status group dataid configinfo))))]
+    (update-conf! manager)
     (update-status group dataid
                    {gmger manager
                     gconf (.getAvailableConfigureInfomation manager 1000)})))
